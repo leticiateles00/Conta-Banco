b97d010f70ed63ed5a870427cb0d3d1a215393b7
leticiateles00 <correa.pires1980@gmail.com>
2025-08-11 14:46:16 -0300

    projeto conta terminal em java

---
+++ contaTerminal.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class contaTerminal
+{
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número do Usuário: ");
+        var usuario = scanner.nextInt();
+        System.out.println("Por favor, digite o número da Agência: ");
+        var agencia = scanner.nextInt();
+        System.out.println("Por favor, digite o seu nome: ");
+        var nome = scanner.nextLine();
+        System.out.println("Por favor, digite o número da Agência: ");
+        var agencia = scanner.nextInt();
+
+
+
+
+    }
+}
